913dc02a3862c0e9302353701e70ade0c7a906cd
rfermann <18217019+rfermann@users.noreply.github.com>
2026-03-25 17:28:59 +0100

    add path

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter(), paths: { base: '/app', assets: '/app' } },
+	kit: { adapter: adapter(), paths: { base: '/app' } },
 	optimizeDeps: {
     exclude: ['@lucide']
 },
